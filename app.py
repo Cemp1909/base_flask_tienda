ca4c7ce8cad9bf4ebfe12e448e24e61c43516bc7
Camilo Mora <camilomora@MacBook-Air-de-Camilo.local>
2025-10-09 17:28:15 -0500

    Agregar Procfile y ajustes para Render

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ db.init_app(app)
 app.register_blueprint(main)
 
 if __name__ == "__main__":
+    from app import app
     with app.app_context():
-        db.create_all()   # ✅ crea cualquier tabla que falte (incluida transactions)
-    app.run(debug=True)
-
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
 
